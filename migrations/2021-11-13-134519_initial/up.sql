40a87ad6881e08756238c13d242d2990152c0b31
Tomáš Janoušek <tomaszbrna@gmail.com>
2021-11-13 16:33:56 +0100

    return clause

--- migrations/2021-11-13-134519_initial/up.sql
+++ migrations/2021-11-13-134519_initial/up.sql
@@ -13,6 +13,8 @@ BEGIN
 
     INSERT INTO stonker (name, balance) VALUES ('walt', 70) returning id INTO walt;
     INSERT INTO company (name, performer_id) VALUES ('disney', walt);
+
+    RETURN 6;  -- number of changed columns 
 END;
 $$ LANGUAGE plpgsql;
 SELECT generate_data();
