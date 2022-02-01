613bffc85cc79cd9767d8ec6e25e5b5a4968678c
Tomas Janousek <tomaszbrna@gmail.com>
2022-02-01 19:19:16 +0100

    Removed unused counter

--- client/src/components/mod.rs
+++ client/src/components/mod.rs
@@ -1,4 +1,3 @@
-pub mod counter;
 pub mod nav_element;
 pub mod home_page;
 pub mod company_page;
@@ -6,4 +5,4 @@ pub mod news_card;
 pub mod company;
 pub mod register_modal;
 
-pub use self::{counter::Counter, nav_element::NavElement, news_card::NewsCard, company::Company, register_modal::RegisterModal};
+pub use self::{nav_element::NavElement, news_card::NewsCard, company::Company, register_modal::RegisterModal};
